79286c88a812537ea7ea1e94055498b24a78a2a1
dima-kulikov <skaizy44@gmail.com>
2021-05-14 18:35:31 +0300

    ыф
    ыф

--- src/js/main.js
+++ src/js/main.js
@@ -7,3 +7,13 @@ document.getElementById('container').innerHTML = test.init();
 document.getElementById('filter').addEventListener('click', () => {
     console.log(test.filter('1'));
 })
+
+// Фильтр цены от и до
+
+const btnPrice = document.querySelector('#btn__price');
+let minPrice = document.getElementById("min-price");
+let maxPrice = document.getElementById("max-price");
+
+console.log(minPrice.value);
+
+btnPrice.addEventListener('click', ()=>{});
