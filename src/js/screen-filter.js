17e61f1a04eba1c944babfaf02b7abda9290f7bc
dima-kulikov <skaizy44@gmail.com>
2021-05-20 11:06:17 +0300

    fds
    dsf

--- src/js/screen-filter.js
+++ src/js/screen-filter.js
@@ -1,27 +1,66 @@
 import data from '../data/testData.json';
 import testTemplate from '../templates/test.hbs';
 
-
-const inFirst = document.querySelector('#first');
-const inSecond = document.querySelector('#second');
-const inThird = document.querySelector('#third');
-const btnInputFilter = document.querySelector('#in-filter');
 const testContainer = document.querySelector('.test-container');
+const btnInputFilter = document.querySelector('#in-filter');
+const checkboxGroup = document.getElementById('checkbox-group');
 
-btnInputFilter.addEventListener('click', () =>{
-    if(first.checked){
-    const firstAnsver = data.filter(us=> us.screen == 13);
-        testContainer.innerHTML = testTemplate({ users: firstAnsver })
-     }
-     if(second.checked){
-    const secondAnsver = data.filter(us=> us.screen == 15.6);
-        testContainer.innerHTML = testTemplate({ users: secondAnsver })
-     }
-     if(third.checked){
-    const thirdAnsver = data.filter(us=> us.screen == 18);
-        testContainer.innerHTML = testTemplate({ users: thirdAnsver })
-     }
-     else{
-        testContainer.innerHTML = testTemplate({ users: data })
-     }
+const ids = [];
+
+const handleCheckbox = (e) => {
+   const { target } = e;
+
+   if (target.nodeName === 'INPUT') {
+      if (target.checked) {
+         ids.push(target.value);
+      } else {
+         const index = ids.indexOf(target.value);
+
+         ids.splice(index, 1);
+      } 
+   }
+};
+
+const handleFilter = () => {
+   const result = [];
+
+   if (!ids.length) return data
+
+   ids.forEach(id => {
+      result.push(...data.filter(item => item.screen === id))
+   })
+
+   return result
+};
+
+btnInputFilter.addEventListener('click', () => {
+   testContainer.innerHTML = testTemplate({ users: handleFilter() })
 });
+checkboxGroup.addEventListener('click', handleCheckbox);
+
+
+
+// -----------------------------------------
+// const inFirst = document.querySelector('#first');
+// const inSecond = document.querySelector('#second');
+// const inThird = document.querySelector('#third');
+
+
+
+// btnInputFilter.addEventListener('click', () =>{
+//     if(inFirst.checked){
+//     const firstAnsver = data.filter(us=> us.screen == 13);
+//         testContainer.innerHTML = testTemplate({ users: firstAnsver })
+//      }
+//      if(inSecond.checked){
+//     const secondAnsver = data.filter(us=> us.screen == 15.6);
+//         testContainer.innerHTML = testTemplate({ users: secondAnsver })
+//      }
+//      if(inThird.checked){
+//     const thirdAnsver = data.filter(us=> us.screen == 18);
+//         testContainer.innerHTML = testTemplate({ users: thirdAnsver })
+//      }
+//      else{
+//       //   testContainer.innerHTML = testTemplate({ users: data })
+//      }
+// });
